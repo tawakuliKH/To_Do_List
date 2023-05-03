c28fcfe567801cbd0e41c233b5483df0c0a7e9f9
tawakuliKH <tawakuli456@gmail.com>
2023-05-03 22:53:22 +0430

    Update index.js for read and delet functions

--- src/index.js
+++ src/index.js
@@ -2,33 +2,7 @@ import './style.css';
 import './assets/images/reload.png';
 import './assets/images/enter.png';
 import './assets/images/more.png';
+import './assets/images/trash.png';
+import display from './manipulation';
 
-const tasks = [
-  {
-    desc: 'Wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    desc: 'Complete The Microverse',
-    completed: false,
-    index: 2,
-  },
-  {
-    desc: 'Go to a travel',
-    completed: false,
-    index: 3,
-  },
-  {
-    desc: 'Find a good job',
-    completed: false,
-    index: 4,
-  },
-];
-const items = document.querySelector('.items');
-tasks.forEach((task) => {
-  items.innerHTML += `<div class="item">
-                <div class="item-desc"><div class="check"><input type="checkbox" class="checkbox"></div><div class="desc">${task.desc}</div></div>
-                <div class="humberger"><img class="humberger-image" src="assets/images/more.png" alt="humberger"></div>
-               </div>`;
-});
+display();
