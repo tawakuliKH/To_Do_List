f00b7801ec560e73e3c3e0cf4c14dc2970c82b48
tawakuliKH <tawakuli456@gmail.com>
2023-05-04 22:03:58 +0430

    Update manipulation.js file for interective list, complete All button

--- src/manipulation.js
+++ src/manipulation.js
@@ -1,4 +1,4 @@
-import Todo from './todo.js';
+import Todo from './todo';
 
 // Display list
 const display = () => {
@@ -14,10 +14,17 @@ const display = () => {
         index: localtask.index,
       };
       task.tasks.push(tt);
-      items.innerHTML += `<form class='item item${localtask.index}'>
-      <div class='item-desc'><div class='check'><input type='checkbox' id='check${localtask.index}' class='checkbox'></div><input type='text' id='desc${localtask.index}' class='desc' value='${localtask.desc}'></div>
+      if (localtask.completed === true) {
+        items.innerHTML += `<form class='item item${localtask.index}'>
+      <div class='item-desc'><div class='check'><input type='checkbox' id='check${localtask.index}' class='checkbox checkbox${localtask.index}' checked></div><input type='text' id='desc${localtask.index}' class='desc completed desc${localtask.index}' value='${localtask.desc}'></div>
       <div class='humberger'><img class='humberger-image humberger-image${localtask.index}' src='assets/images/more.png' alt='humberger'> <img class='trash-image trash-image${localtask.index}' src='assets/images/trash.png' alt='trash'></div>
       </form>`;
+      } else {
+        items.innerHTML += `<form class='item item${localtask.index}'>
+      <div class='item-desc'><div class='check'><input type='checkbox' id='check${localtask.index}' class='checkbox checkbox${localtask.index}'></div><input type='text' id='desc${localtask.index}' class='desc desc${localtask.index}' value='${localtask.desc}'></div>
+      <div class='humberger'><img class='humberger-image humberger-image${localtask.index}' src='assets/images/more.png' alt='humberger'> <img class='trash-image trash-image${localtask.index}' src='assets/images/trash.png' alt='trash'></div>
+      </form>`;
+      }
     });
   }
 
@@ -42,6 +49,20 @@ const display = () => {
   // reload
   const reloadBtn = document.querySelector('.reload-image');
   task.reload(reloadBtn);
+
+  // Completed
+  const check = document.querySelectorAll('.checkbox');
+  task.completed(check);
+
+  // Clear All
+  const btnClear = document.querySelector('.btn-clear');
+  const checkedIndex = [];
+  task.tasks.forEach((completedTask, index) => {
+    if (completedTask.completed === true) {
+      checkedIndex.push(index);
+    }
+  });
+  task.clearAll(btnClear, checkedIndex);
 };
 
 export default display;
