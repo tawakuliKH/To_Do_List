f607803f37bcf28fdcabe55ee313a7631d91ea39
EddxSotz <96507617+EddxSotz@users.noreply.github.com>
2023-05-10 14:48:24 -0600

    delete task test

--- todo.test.js
+++ todo.test.js
@@ -16,4 +16,15 @@ describe('ToDo', () => {
     todo.addTask('Task 1', false, 0);
     expect(todo.tasks.length).toBe(1);
   });
+  
+  test('delet a task', () => {
+    todo.addTask('Task 1', false, 0);
+    todo.addTask('Task 2', false, 1);
+    todo.addTask('Task 3', false, 2);
+    todo.addTask('Task 4', false, 3);
+    todo.deleteItem(1);
+    todo.deleteItem(2);
+    todo.deleteItem(0);
+    expect(todo.tasks.length).toBe(1);
+  });
 });
